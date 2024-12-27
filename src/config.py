32d9ee1ed0bd222e25180828f1e5ea709380ed46
sjrom47 <sergiojimenezromero4@gmail.com>
2024-12-27 18:41:37 +0100

    V1 of classifier

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
 PIECE_TYPES = ["pawn", "knight", "bishop", "rook", "queen", "king"]
 LABELED_IMAGES_DIR = "./data/labeled_data"
 UNLABELED_IMAGES_DIR = "./data/unlabeled_data"
+DATASET_DIR = "./data/labeled_data"
+CLASSIFIER_DIR = "./data/classifier"
