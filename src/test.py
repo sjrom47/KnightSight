40e0102fafb01775d7c93846be57550fc4df729a
sjrom4 <sjrom4@gmail.com>
2024-12-30 14:17:57 +0100

    Raspberry images

--- src/test.py
+++ src/test.py
@@ -5,29 +5,29 @@ import time
 import glob
 
 # Initialize the camera
-# picam2 = Picamera2()
+picam2 = Picamera2()
 
-# # Configure the camera
-# picam2.configure(picam2.create_still_configuration())
+# Configure the camera
+picam2.configure(picam2.create_still_configuration())
 
-# # Start the camera
-# picam2.start()
+# Start the camera
+picam2.start()
 
-# # Capture 5 images
-# for i in range(20):
-#     filename = f'../data/calibration_photos/image_{i+1}.jpg'
-#     picam2.capture_file(filename)
-#     print(f'Captured {filename}')
-#     time.sleep(3)  # Wait for 1 second between captures
+# Capture 5 images
+for i in range(3):
+    filename = f"data/photos/extra/image_{i+1}.jpg"
+    picam2.capture_file(filename)
+    print(f"Captured {filename}")
+    time.sleep(3)  # Wait for 1 second between captures
 
-# # Stop the camera
-# picam2.stop()
+# Stop the camera
+picam2.stop()
 
-dim = (7, 7)
+# dim = (7, 7)
 
-imgs_path = [i for i in glob.glob("../data/calibration_photos/image_*.jpg")]
-imgs = [cv2.imread(i) for i in imgs_path]
-print(imgs)
+# imgs_path = [i for i in glob.glob("../data/calibration_photos/image_*.jpg")]
+# imgs = [cv2.imread(i) for i in imgs_path]
+# print(imgs)
 
-rms, intrinsics, extrinsics, dist_coeffs = calibrate_camera(imgs, dim)
-print(rms)
+# rms, intrinsics, extrinsics, dist_coeffs = calibrate_camera(imgs, dim)
+# print(rms)
